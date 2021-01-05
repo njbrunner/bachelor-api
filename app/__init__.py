ffad62ea681dd231565a426292beb474282500ee
Nick Brunner <njbrunner@gmail.com>
2021-01-04 20:45:26 -0500

    New database, configured dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -5,34 +5,31 @@ from flask_pymongo import PyMongo
 from flask_mongoengine import MongoEngine
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
 
-    JWT_SECRET_KEY = 'super-secret'
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
     TESTING = True
 
-    # RetryableWrites are unsupported for mLab MongoDB
-    MONGO_URI = "mongodb://localhost:27017/bachelor?retryWrites=false"
-    MONGODB_SETTINGS = {
-        'host': MONGO_URI
-    }
+    MONGO_URI = os.getenv("MONGO_URI")
+    MONGODB_SETTINGS = {"host": MONGO_URI}
 
 
 class ProductionConfig(BaseConfig):
-    # RetryableWrites are unsupported for mLab MongoDB
-    MONGO_URI = os.environ.get('MONGODB_URI')
+    MONGO_URI = os.environ.get("MONGODB_URI")
     if MONGO_URI:
         MONGO_URI = MONGO_URI + "?retryWrites=false"
-    MONGODB_SETTINGS = {
-        'host': MONGO_URI
-    }
+    MONGODB_SETTINGS = {"host": MONGO_URI}
 
 
 def create_app():
@@ -40,7 +37,7 @@ def create_app():
     # CREATE app
     app = Flask(__name__)
 
-    if os.environ.get('MONGODB_URI'):
+    if os.environ.get("MONGODB_URI"):
         app.config.from_object(ProductionConfig)
     else:
         app.config.from_object(DevelopmentConfig)
@@ -51,20 +48,21 @@ def create_app():
 
     return app
 
-def initialize_extensions(app):# CORS
-    # Create token manager
+
+def initialize_extensions(app):
     CORS(app)
     mongo = PyMongo(app)
     db = MongoEngine(app)
     jwt = JWTManager(app)
+
 
 def register_blueprints(app):
     from app.routes import hello
     from app.routes import contestant_routes
     from app.routes import player_routes
     from app.routes import auth
+
     app.register_blueprint(hello.HELLO_BP)
     app.register_blueprint(contestant_routes.CONTESTANT_BP)
     app.register_blueprint(player_routes.PLAYER_BP)
     app.register_blueprint(auth.AUTH_BP)
-
